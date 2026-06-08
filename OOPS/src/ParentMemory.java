f36af1d32aee1614c3be8298c8cc7c72b593c61a
Tarak2903 <tarakkhurana29@gmail.com>
2026-06-08 21:36:58 +0530

    Interface

--- OOPS/src/ParentMemory.java
+++ OOPS/src/ParentMemory.java
@@ -1,3 +1,40 @@
-public class ParentMemory {
+/*
+IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!
 
+VARIABLES ARE TAKEN INTO ACC BY REFERENCE TYPE NOT  OBJECT TYPE
+
+FOR EG
+
+Animal a= new Dog();
+
+here a.x will print the x of  Animal
+ */
+
+class Parent{
+    Parent(){
+        show();
+    }
+
+    void show(){
+        System.out.println("Hello i am Parent");
+    }
 }
+class Child extends Parent{
+    int x;
+    Child(int x){
+        this.x=x;
+    }
+    void show(){
+        System.out.println("Hello is my name is "+x);
+    }
+}
+
+public class ParentMemory {
+     static void main(String []args){
+         System.out.println("Tarak is my name");
+         Child x= new Child(10);
+         System.out.println(x.x);
+    }
+}
+
+//Here the method overriding that i am doing is called dynamic dispatch
