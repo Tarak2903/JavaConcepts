91fcd1dce035862521208c3854abb3e4a4ff9eb5
Tarak2903 <tarakkhurana29@gmail.com>
2026-06-09 21:55:27 +0530

    Interface

--- OOPS/src/ObjectClass.java
+++ OOPS/src/ObjectClass.java
@@ -1,11 +1,19 @@
 class Student{
-
+    int id;
+    Student (int id){
+        this.id=id;
+    }
 }
 public class ObjectClass {
     public static void main(String []args){
-        Student s= new Student();
-        System.out.println(s);
-        // outputs Student@8efb846 -> ClassName @HexDecimalCode
+        Student s1= new Student(1);
+        Student s2=new Student(1);
+//        System.out.println(s);
+//        // outputs Student@8efb846 -> ClassName @HexDecimalCode
+
+
+        System.out.println(s1.equals(s2));
+        //For object default is comparing of reference that is why it shows false
     }
 }
 
