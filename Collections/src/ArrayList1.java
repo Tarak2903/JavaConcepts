00193522e4bef2d923a93fd8cff48800a5ae868f
Tarak2903 <tarakkhurana29@gmail.com>
2026-06-15 15:46:25 +0530

    Collections Hierarchy uploaded

--- Collections/src/ArrayList1.java
+++ Collections/src/ArrayList1.java
@@ -1,10 +1,33 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
+class ListIteratorLearning{
+    public void hello() {
+        List<Integer> ls2 = new ArrayList<>();
+        ls2.add(1);
+        ls2.add(2);
+        ls2.add(3);
+        ls2.add(4);
+        ls2.add(5);
+        ListIterator<Integer> x= ls2.listIterator();
+        while(x.hasNext()) {
+           int z=x.next();
+           x.set(z+1);
+        }
+
+
+       while(x.hasPrevious()){
+           System.out.println(x.previous());
+       }
+    }
+}
 public class ArrayList1{
     public static void main(String[] args){
 
+        ListIteratorLearning le=new ListIteratorLearning();
+        le.hello();
       List<Integer> ls= new ArrayList<>();
       ls.add(1);
       ls.add(2);
@@ -20,9 +43,9 @@ public class ArrayList1{
         }
 
         Iterator<Integer>it2= ls.iterator();
-        while(it2.hasNext()){
-            System.out.println(it2.next()  );
-        }
+//        while(it2.hasNext()){
+//            System.out.println(it2.next()  );
+//        }
 
     }
 }
