a6677d3b10271b02a2f022a15d11f15d83e00571
Tarak2903 <tarakkhurana29@gmail.com>
2026-06-15 21:03:23 +0530

    HashSetImplementation Done

--- Collections/src/HashSetImplementation.java
+++ Collections/src/HashSetImplementation.java
@@ -1,5 +1,36 @@
+import java.util.HashSet;
+
+class Student{
+    int id;
+    Student(int id){
+        this.id=id;
+    }
+    /*
+    Inorder to have unique student with unique id i need to make sure that i override both equals and hashCOde
+     */
+    public boolean equals(Object o){
+        if(this==o)return true;
+
+        if(!(o instanceof Student))return false;
+
+        return this.id==((Student) o).id;
+    }
+
+    public int hashCode(){
+        return id;
+    }
+}
+
 public class HashSetImplementation {
     void main(){
+        HashSet<Student> hashSet= new HashSet<>();
+
+        hashSet.add(new Student(1));
+        hashSet.add(new Student(1));
+
+
+
+        System.out.println(hashSet.size());
 
     }
 }
@@ -10,4 +41,3 @@ public class HashSetImplementation {
 
 // HashSet implements the Set Interface
 // HashSet uses Hashing + Buckets
-
