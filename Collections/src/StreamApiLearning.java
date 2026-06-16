0d2b65a667408770919b3186b0f4b6200be4f041
Tarak2903 <tarakkhurana29@gmail.com>
2026-06-16 14:50:35 +0530

    Stream Hard Learning

--- Collections/src/StreamApiLearning.java
+++ Collections/src/StreamApiLearning.java
@@ -1,29 +1,62 @@
-import java.util.ArrayList;
-import java.util.List;
+import javax.crypto.AEADBadTagException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamApiLearning {
     void main(){
-        List<Integer> ls = new ArrayList<>(List.of(1,2,3,4,5,6,7));
+        List<Integer> ls = new ArrayList<>(List.of(1,2,3,4,5,6,7,7));
 
         ls.stream().
-                filter(x->x%2==0).
-                map(x->x*x)
-                .forEach(System.out::println);
+                distinct()
+                .forEach(System.out::print);
 
         // If someone is using its completely discard and does not move down the stream at all
 
+
+        //FIND EVEN NUMBERS IN A STREAM
         long count=ls.stream().
                 filter(x->x%2==0)
                 .count();
 
-
+        //Find Product of all Elements in a Stream
         int val= ls.stream()
                 .reduce(1,(a,b)->a*b);
 
-
+        //Find minimum in a Stream
         ls.stream().
                 min((a,b)->Integer.compare(a,b))
                 .ifPresent(System.out::println);
+
+
+        //Print first even number
+        ls.stream()
+                .filter(x->x%2==0)
+                .findFirst()
+                .ifPresent(System.out::println);
+
+            List<Integer> ls2 = ls.stream()
+                .filter(x->x%2==0)
+                .toList();// This cannot be modified
+
+        Set<Integer> st = ls.stream()
+                                .collect(Collectors.toSet()); // collect method can be used and it will
+
+        System.out.println("------------------");
+        for(Integer i: st){
+            System.out.print(i+" ");
+        }
+        List<String> words =  List.of("java","aws","spring","docker");
+
+        Map<Integer, List<String>>mp=words
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                String::length
+                        )
+                );
+
+
+
     }
 
 }
@@ -49,7 +82,5 @@ public class StreamApiLearning {
     reduce(...)
 
 NOTHING ACTUALLY EXECUTES BEFORE THE TERMINAL OPERATION THIS IS CALLED LAZY EVALUATION
-
-
 
  */
